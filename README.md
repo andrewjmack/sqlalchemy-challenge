27812c926ba2f2ddef1b1ae5fc4a298c5188a43a
andrewjmack <andrewmack5274@gmail.com>
2024-04-29 18:29:17 -0600

    pushing final files to repo

--- README.md
+++ README.md
@@ -1,9 +1,27 @@
 # sqlalchemy-challenge
+### Andrew Mack | DU Data Analytics | April 2024
 
+## Summary
+This repository contains the files to meet the requirements of the SQL Alchemy module 10 homework challenge.
+
+The challenge consists of two parts: analysis (Jupyter notebook) and endpoints through a Flask server app (Python file). An html template is used to improve the API landing page.
 
 ## Repo Contents
--
-
+- README
+- Surfsup
+-   \climate_starter.ipynb
+-   \app.py
+-   \Resources
+-       \hawaii_measurements.csv
+-       \hawaii_stations.csv
+-       \hawaii.sqlite
+-   \templates
+-       \index.html
 
 ## Resources
-- Strptime() method utilized to convert string to datetime object for  rehttps://www.programiz.com/python-programming/datetime/strptime
+- Initial notebook and python files as well as datasets provided
+- Class instruction and module activities
+- Tutor assistance with html and datetime code implementation
+- HTML template starter code to beautify API landing page: https://getbootstrap.com/
+- Flask documentation for rendering html: https://flask.palletsprojects.com/en/3.0.x/quickstart/#rendering-templates
+- Strptime() method utilized to convert string to datetime object for start and end dates from API endpoint URLs: https://www.programiz.com/python-programming/datetime/strptime
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,14 +1,13 @@
-# Import the dependencies.
-
+# Import the dependencies
 import numpy as np
+import datetime as dt
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-from sqlalchemy import Column, Integer, String, Float
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 #################################################
 # Database Setup
@@ -16,7 +15,6 @@ from flask import Flask, jsonify
 
 # Create engine using the `hawaii.sqlite` database file
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-# print(engine)
 
 # Declare a Base using `automap_base()`
 Base = automap_base()
@@ -32,6 +30,11 @@ Station = Base.classes.station
 # Create a session
 session = Session(bind=engine)
 
+# Calculate the date one year from the last date in data set for later use
+most_recent = session.query(Measurement.date).\
+    order_by(Measurement.date.desc()).first().date
+one_year = dt.datetime.strptime(most_recent, '%Y-%m-%d') - dt.timedelta(days=365)
+
 #################################################
 # Flask Setup
 #################################################
@@ -40,45 +43,174 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+
+# Landing page for available routes
+# render_template for visually improved html landing page
 @app.route("/")
 def welcome():
-    return(
-        f"Welcome to the climate API!<br/>"
-        f"<br/>"
-        f"Available routes:<br/>"
-        f"<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"<br/>"
-        f"/api/v1.0/<start>/<end><br/>"
-    )
+    return render_template("index.html")
 
-"""
-@app.route("/api/v1.0/precipitation")
+# Returns json with date as the key and value as the precipitation for last year in database
+#@app.route("/api/v1.0/precipitation")
+@app.route("/precipitation")
 def precipitation():
-    return jsonify(
+    
+    # print call acknowledgment to terminal
+    print()
+    print("Server received request for 'Precipitation' page...")
+    print()
 
+    # Perform a query to retrieve the date and precipitation scores.
+    prcp_results = session.query(Measurement.date, Measurement.prcp).\
+                                filter(Measurement.date >= one_year).\
+                                order_by(Measurement.date).all()
+
+    # close session
+    session.close()
+
+    # Store results as a dictionary accessible for JSON
+    prcp_dict = dict(prcp_results)
+
+    # Print results to terminal
+    print()
+    print(f"Precipitation data for the last year:")
+    print()
+    print(prcp_dict)
+    print()
+    print(f"---- RESULTS COMPLETE ----")
+    print()
+
+    # Return results for server call
+    return jsonify(prcp_dict)
+
+# Returns jsonified data of all of the stations in the database    
 @app.route("/api/v1.0/stations")
 def stations():
-    return jsonify(
+    
+    # print call acknowledgment to terminal
+    print()
+    print("Server received request for 'Stations' page...")
+    print()
 
+    # query station data
+    stations = session.query(Station.station).all()
+
+    # close session
+    session.close()
+
+    # store station data in accessible form for JSON
+    station_list = list(np.ravel(stations))
+
+    # Print results to terminal
+    print()
+    print(f"The list of stations:")
+    print()
+    print(station_list)
+    print()
+    print(f"---- RESULTS COMPLETE ----")
+    print()
+
+    # Return results for server call
+    return jsonify(station_list)
+
+# Returns jsonified TOBS data for the most active station (USC00519281) for the last year
 @app.route("/api/v1.0/tobs")
 def tobs():
-    return jsonify(
+    
+    # print call acknowledgment to terminal
+    print()
+    print("Server received request for 'TOBS' page...")
+    print()
 
+    # query temperature observations for most active station
+    one_year_tobs = session.query(Measurement.date, Measurement.tobs).\
+                filter(Measurement.station == 'USC00519281').\
+                filter(Measurement.date > one_year).all()
+    
+    # close session
+    session.close()
+
+    # store results as a dictionary accessible for JSON
+    tobs_dict = dict(one_year_tobs)
+
+    # Print results to terminal
+    print()
+    print(f"Temperature observations for station USC00519281 for the last year:")
+    print()
+    print(tobs_dict)
+    print()
+    print(f"---- RESULTS COMPLETE ----")
+    print()
+
+    # Return results for server call
+    return jsonify(tobs_dict)
+
+# Return a JSON list of the minimum temperature, the average temperature,
+# and the maximum temperature for a specified start
 @app.route("/api/v1.0/<start>")
-def start():
-    return jsonify(
+def start(start):
+    
+    # print call acknowledgment to terminal
+    print()
+    print(f"Server received request for 'TOBS' page with start date of: {start}")
+    print()
 
+    # ensure start from URL parameter is in datetime format (not string)
+    start = dt.datetime.strptime(start, "%Y%m%d")
+
+    # query data since provided start date
+    tobs_start = session.query(func.min(Measurement.tobs),\
+                    func.max(Measurement.tobs),\
+                    func.avg(Measurement.tobs)).\
+                    filter(Measurement.date >= start).all()
+    
+    # close session
+    session.close()
+
+    # store station data in accessible form for JSON
+    tobs_start_list = list(np.ravel(tobs_start))
+
+    # Print results to terminal
+    print()
+    print(f"Minimum, maximum and average temperature observations since {start}:")
+    print()
+    print(tobs_start_list)
+    print()
+    print(f"---- RESULTS COMPLETE ----")
+    print()
+
+    # Return results for server call
+    return jsonify(tobs_start_list)
+
+# Return a JSON list of the minimum temperature, the average temperature,
+# and the maximum temperature for a specified start-end range
 @app.route("/api/v1.0/<start>/<end>")
-def start_end():
-    return jsonify()
-"""
+def start_end(start, end):
+    
+    # print call acknowledgment to terminal
+    print()
+    print(f"Server received request for 'TOBS' page with start date of: {start}")
+    print()
+    
+    # ensure start from URL parameter is in datetime format (not string)
+    start = dt.datetime.strptime(start, "%Y%m%d")
+    end = dt.datetime.strptime(end, "%Y%m%d")
+
+    # query min, max and average TOBS from given start date to end date
+    tobs_start_end = session.query(func.min(Measurement.tobs),\
+                    func.max(Measurement.tobs),\
+                    func.avg(Measurement.tobs)).\
+                    filter(Measurement.date >= start).\
+                    filter(Measurement.date <= end).all()
+    
+    # close session
+    session.close()
+
+    # store station data in accessible form for JSON
+    tobs_start_end_list = list(np.ravel(tobs_start_end))
+
+    # Return results for server call
+    return jsonify(tobs_start_end_list)
     
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
